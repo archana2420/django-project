cc3b26cf9ae435549d1c175d0a5ce05acd356722
Archana S Nair <archanasn20@gmail.com>
2023-02-12 02:17:57 +0530

    user login and loggout complete

--- ShoppingCart/products/models.py
+++ ShoppingCart/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.URLField(max_length=100000,default="https://picsum.photos/200")
--- ShoppingCart/users/views.py
+++ ShoppingCart/users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.authentication import SessionAuthentication,BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from .serializers import UserSerializer,RegisterSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
 # Create your views here.
+
+class RegisterUserAPI(generics.CreateAPIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = '/dashboard/signup.html'
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ListAllUsersAPI(generics.ListAPIView):
+    authentication_classes = [SessionAuthentication,BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UpdateUserAPI(generics.UpdateAPIView):
+    authentication_classes = [SessionAuthentication,BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+  
+
+class DeleteUserAPI(generics.DestroyAPIView):
+    authentication_classes = [SessionAuthentication,BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+    
+
